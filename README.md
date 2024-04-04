2821f0afcc6a42c518876f4c01bb673a854507f9
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-04-04 18:54:13 +0100

    Add cell transparency, sounds, grid resizing options

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ An in-browser implementation of an Etch-a-sketch to practice DOM manipulation
 - [X] HTML structure
 - [X] CSS styling
 - [X] JS 16x16 div creation
-- [ ] JS grid draw logic (start off with black)
-- [ ] Extra: Randomise color
-- [ ] Extra: Add gravity to squares
+- [X] JS grid draw logic (start off with black)
+- [X] Add resize grid option (input and apply button to redraw grid)
+- [X] Extra: Randomise color
+- [ ] Extra: Allow option to pick color
--- main.js
+++ main.js
@@ -1,22 +1,103 @@
-// load sketch
+const grid = document.querySelector('.grid')
 
-// header styling
-let prevScrollpos = window.scrollY;
+let xSize = 16;
+let ySize = 16;
 
-const headerDiv = document.querySelector("header")
-const headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+const btnReset = document.querySelector('.btn-reset')
+btnReset.addEventListener("click", () => {
+    resetGrid();
+})
 
-window.onscroll = function() {
-   let currentScrollPos = window.scrollY;
+const btnSize1 = document.getElementById('size1');
+btnSize1.addEventListener("click", () => {
+    if (xSize != 16) {
+        xSize = ySize = 16;
+        resetGrid();
+    }
+})
+const btnSize2= document.getElementById('size2');
+btnSize2.addEventListener("click", () => {
+    if (xSize != 32) {
+    xSize = ySize = 32;
+    resetGrid();
+    }
+})
+const btnSize3= document.getElementById('size3');
+btnSize3.addEventListener("click", () => {
+    if (xSize != 64) {
+    xSize = ySize = 64;
+    resetGrid();
+    }
+})
 
-  /* if we're scrolling up, or we haven't passed the header, show the header */
-  if (prevScrollpos > currentScrollPos  || currentScrollPos < headerBottom){  
-      headerDiv.style.top = "0";
-  }
-  else{
-      /* otherwise we're scrolling down & have passed the header so hide it */
-      headerDiv.style.top = "-5.2rem";
-  } 
+createGrid();
+activateColors();
 
-  prevScrollpos = currentScrollPos;
+function dropMenu() {
+    document.getElementById("id-dropdown").classList.toggle("show");
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+function createGrid(xAxis=16,yAxis=16) {
+    for (let i = 0; i < yAxis; i++) {
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        for (let k = 0; k < xAxis; k++) {
+            let gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            gridRow.appendChild(gridCell);
+        }
+        grid.appendChild(gridRow);
+    }
+}
+
+function activateColors() {
+    const gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            let cellAlpha = 10;
+            if (cell.textContent === '' || cell.textContent === '0') {
+               cell.textContent = '10';
+            } else {
+                cellAlpha = Number(cell.textContent) - 1;
+            }
+            cell.textContent = cellAlpha;
+            cell.style.backgroundColor = getRandomColor(cellAlpha*0.1);
+            getAudio(cell);
+        });
+    });
+}
+
+// audio sourced from Pixabay. Author: floraphonic
+function getAudio(element) {
+    audioArray = ["pop1","pop2","pop3","pop4","pop5","pop6"];
+    let rng = Math.floor(Math.random()*audioArray.length);
+    let audio = document.createElement("audio");
+    audio.src = "audio/" + audioArray[rng] + ".mp3";
+    element.appendChild(audio)
+    audio.volume = 0.01;
+    audio.play();
+}
+
+function getRandomColor(alpha) {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 70%,' + alpha + ')';
+}
+
+function resetGrid() {
+    grid.innerHTML = '';
+    createGrid(xSize,ySize);
+    activateColors();
+}
+
